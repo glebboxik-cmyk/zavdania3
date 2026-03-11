7c62b16d3d2935bf4fe1859a8131a0422b44dd89
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-11 14:34:50 +0200

    Implement main method for MainTest class

--- src/main/java/ex02/MainTest.java
+++ src/main/java/ex02/MainTest.java
@@ -1,4 +1,14 @@
 package ex02;
 
 public class MainTest {
+    public static void main(String[] args) {
+        ViewResult vr = new ViewResult();
+        vr.viewInit();
+
+        if (vr.getItems().size() > 0) {
+            System.out.println("Test Collection: ok");
+        } else {
+            System.out.println("Test Collection: Pomylka");
+        }
+    }
 }
