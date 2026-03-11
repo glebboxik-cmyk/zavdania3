c7a1a0ff549b2443f9cd3701bf18336a6f16c53b
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-11 14:34:27 +0200

    Implement main method for Viewable functionality

--- src/main/java/ex02/Main.java
+++ src/main/java/ex02/Main.java
@@ -1,4 +1,18 @@
 package ex02;
 
 public class Main {
+    public static void main(String[] args) {
+        Viewable factory = new ViewableResult();
+        View v = factory.getView();
+
+        v.viewInit();
+        v.viewShow();
+        try {
+            v.viewSave();
+            v.viewRestore();
+            v.viewShow();
+        } catch (Exception e) {
+            System.out.println("Pomylka: " + e.getMessage());
+        }
+    }
 }
